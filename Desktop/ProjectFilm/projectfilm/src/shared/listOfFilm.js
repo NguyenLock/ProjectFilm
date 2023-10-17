0c8a46c6d090d7ce55fca49cecc4e0610ab14017
NguyenLock <nlocnguyen9@gmail.com>
2023-10-17 14:32:00 +0700

    img film update

---
+++ Desktop/ProjectFilm/projectfilm/src/shared/listOfFilm.js
@@ -0,0 +1,65 @@
+export const data = [
+   {
+    Id: '1',
+    Image: '',
+    Title: 'Eternals',
+    Year: '2021',
+    Nation: 'USA'
+   },
+   {
+    Id: '2',
+    Image: '',
+    Title: 'The Road',
+    Year: '2009',
+    Nation: 'USA'
+   },
+   {
+    Id: '3',
+    Image: '',
+    Title: 'Dead Man',
+    Year: '1995',
+    Nation: 'USA'
+   },
+   {
+    Id: '4',
+    Image: '',
+    Title: 'The Evil Dead',
+    Year: '1981',
+    Nation: 'USA'
+   },
+   {
+    Id: '5',
+    Image: '',
+    Title: 'Five Night At Freddy',
+    Year: '2023',
+    Nation: 'USA'
+   },
+   {
+    Id: '6',
+    Image: '',
+    Title: 'oppenheimer',
+    Year: '2023',
+    Nation: 'USA'
+   },
+   {
+    Id: '7',
+    Image: '',
+    Title: 'Beyond Utopia',
+    Year: '2023',
+    Nation: ''
+   },
+   {
+    Id: '8',
+    Image: '',
+    Title: 'News of the World',
+    Year: '2020',
+    Nation: 'USA'
+   },
+   {
+    Id: '9',
+    Image: '',
+    Title: 'Ready Or Not',
+    Year: '2019',
+    Nation: 'USA'
+   }
+] 
